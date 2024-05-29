3c6a82021b5eac1cf1b2e97013a6c0f2b4dffeb1
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-05-29 13:41:21 +0000

    Add output from ninja build system too

--- default.nix
+++ default.nix
@@ -16,6 +16,7 @@ pkgs.clangStdenv.mkDerivation {
     mkdir -p $out/bin
     cp test $out/bin
     cp compile_commands.json $out
+    ninja -t compdb > $out/ninja_compile_commands.json
   '';
   cmakeFlags = [
     "-DCMAKE_EXPORT_COMPILE_COMMANDS=1"
